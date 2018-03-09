f6c7fed74ffd60e0b6b1bee9620b231b1a00e8a6
George Spake <gpspake@gmail.com>
2018-03-09 12:59:08 -0600

    create simple script

---
+++ index.js
@@ -0,0 +1,9 @@
+function addTwo (n) {
+  return n + 2;
+}
+
+function multiplyByFour (n) {
+  return n * 4;
+}
+
+console.log("two plus two equals " + addTwo(2) + " and two times four equals " + multiplyByFour(2));
